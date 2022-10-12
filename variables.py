66ae64568690825275d8ee93b653712af934833f
Dangolabhaya99 <abhayadangol100@gmail.com>
2022-10-12 17:11:40 +0545

    First

---
+++ variables.py
@@ -0,0 +1,10 @@
+# named memory location ;where 
+# We can store,update and access value
+roll=input('Enter Roll no:')
+name=input('Enter Name:')
+a=input('sub1:')
+b=input('sub2:')
+a=int(a)
+b=int(b)
+total=a+b
+average=(a+b)/2
